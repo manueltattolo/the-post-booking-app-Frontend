edc551c3e10b59e397f1c71059d54edc669ffae5
shubhamdixit863 <shubhamdixit863@gmail.com>
2021-12-01 22:26:21 +0530

    Added booking stepper

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import BookingPage from './pages/BookingPage';
-
+import BookingStepper from './pages/BookingStepper';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container'
 function App() {
   return (
     <div className="App">
-        <BookingPage/>
+       <Container fixed>
+      </Container>
+        <BookingStepper/>
     </div>
   );
 }
---
+++ src/pages/BookingStepper.js
@@ -0,0 +1,154 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepButton from '@mui/material/StepButton';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import DateRangeIcon from '@mui/icons-material/DateRange';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import PersonIcon from '@mui/icons-material/Person';
+import PercentIcon from '@mui/icons-material/Percent';
+import Calendar from './partials/Calendar';
+import TimePicker from './partials/Timepicker';
+import Details from './partials/Details';
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+const steps = [<DateRangeIcon/>, <AccessTimeIcon/>, <PersonIcon/>,<PercentIcon/>];
+const components=[<Calendar/>,<TimePicker/>,<Details/>];
+
+export default function BookingStepper() {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [completed, setCompleted] = React.useState({});
+
+  const totalSteps = () => {
+    return steps.length;
+  };
+
+  const completedSteps = () => {
+    return Object.keys(completed).length;
+  };
+
+  const isLastStep = () => {
+    return activeStep === totalSteps() - 1;
+  };
+
+  const allStepsCompleted = () => {
+    return completedSteps() === totalSteps();
+  };
+
+  const handleNext = () => {
+    const newActiveStep =
+      isLastStep() && !allStepsCompleted()
+        ? // It's the last step, but not all steps have been completed,
+          // find the first step that has been completed
+          steps.findIndex((step, i) => !(i in completed))
+        : activeStep + 1;
+    setActiveStep(newActiveStep);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStep = (step) => () => {
+    setActiveStep(step);
+  };
+
+  const handleComplete = () => {
+    const newCompleted = completed;
+    newCompleted[activeStep] = true;
+    setCompleted(newCompleted);
+    handleNext();
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+    setCompleted({});
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }} style={{marginTop:"100px"}}>
+       
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        <Item>
+
+            <img width="400px" height="400px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI-YzjyDMrMX94_raTSVaVosTpTWuYc_AR0tK-NUCnPoPxycj3fNO6NhGTmCtypanoO0U&usqp=CAU"/>
+        </Item>
+      </Grid>
+      <Grid item xs={6}>
+      <Box sx={{ width: '100%' }}>
+      <Stepper nonLinear activeStep={activeStep}>
+        {steps.map((label, index) => (
+          <Step key={label} completed={completed[index]}>
+            <StepButton color="inherit" onClick={handleStep(index)}>
+              {label}
+            </StepButton>
+        
+          </Step>
+        ))}
+      </Stepper>
+      <div>
+        {allStepsCompleted() ? (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you&apos;re finished
+            </Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleReset}>Reset</Button>
+            </Box>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+              {
+                  components[activeStep]
+              }
+             
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Button
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Back
+              </Button>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleNext} sx={{ mr: 1 }}>
+                Next
+              </Button>
+              {activeStep !== steps.length &&
+                (completed[activeStep] ? (
+                  <Typography variant="caption" sx={{ display: 'inline-block' }}>
+                    Step {activeStep + 1} already completed
+                  </Typography>
+                ) : (
+                  <Button onClick={handleComplete}>
+                    {completedSteps() === totalSteps() - 1
+                      ? 'Finish'
+                      : 'Complete Step'}
+                  </Button>
+                ))}
+            </Box>
+          </React.Fragment>
+        )}
+      </div>
+    </Box>
+    
+      </Grid>
+     
+    </Grid>
+  </Box>
+  
+  );
+}
